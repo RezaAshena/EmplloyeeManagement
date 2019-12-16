d99160a1a67177ded48d4c0842e89f7164362f60
RezaAshena <ashena.reza@gmail.com>
2019-12-16 10:42:19 -0500

    Get Name Process instead Hello word.

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace EmplloyeeManagement
 			{
 				endpoints.MapGet("/", async context =>
 							{
-						await context.Response.WriteAsync("Hello World!");
+						await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
 					});
 			});
 		}
