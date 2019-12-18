847c875e5c02fd71a9c8eec44881686e50db3f0f
RezaAshena <ashena.reza@gmail.com>
2019-12-18 10:47:11 -0500

    Implement Model validation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,10 +41,15 @@ namespace EmplloyeeManagement.Controllers
 		}
 
 		[HttpPost]
-		public RedirectToActionResult Create(Employee employee)
+		public IActionResult Create(Employee employee)
 		{
-			Employee newEmployee =  _employeeRepository.Add(employee);
-			return RedirectToAction("details", new { id = newEmployee.Id });
+			if(ModelState.IsValid)
+			{
+         Employee newEmployee =  _employeeRepository.Add(employee);
+			   return RedirectToAction("details", new { id = newEmployee.Id });
+			}
+			return View();
+			
 		}
 	}
 } 
