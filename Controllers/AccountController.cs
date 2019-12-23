36b68c8a66e4724823c6bf88b5990bdca742a820
RezaAshena <ashena.reza@gmail.com>
2019-12-23 15:47:57 -0500

    Identity UserManager,SigninManager. Inject them to Account Controller to Create user and SignIn to application.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,16 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmplloyeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmplloyeeManagement.Controllers
 {
-    public class AccountController : Controller
-    {
-			  [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-    }
+	public class AccountController : Controller
+	{
+		private readonly UserManager<IdentityUser> usermanager;
+		private readonly SignInManager<IdentityUser> signInManager;
+
+		public AccountController(UserManager<IdentityUser> usermanager,
+																SignInManager<IdentityUser> signInManager)
+		{
+			this.usermanager = usermanager;
+			this.signInManager = signInManager;
+		}
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+				var result = await usermanager.CreateAsync(user, model.Password);
+				if (result.Succeeded)
+				{
+					await signInManager.SignInAsync(user, isPersistent: false);
+					return RedirectToAction("index", "home");
+
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+			}
+			return View(model);
+		}
+	}
 }
