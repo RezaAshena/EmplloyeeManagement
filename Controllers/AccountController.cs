6e2b95adb3f445168d633ba56d33ac2803a3f180
Reza Ashena <Ashena.reza@gmail.com>
2020-01-08 15:40:27 -0500

    Add Password to local Account Linked to External Login.and show Manage to all user

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -19,8 +19,58 @@ namespace EmplloyeeManagement.Controllers
         private readonly ILogger<AccountController> logger;
 
         [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await userManager.GetUserAsync(User);
+
+            var userHasPassword = await userManager.HasPasswordAsync(user);
+
+            if (userHasPassword)
+            {
+                return RedirectToAction("ChangePassword");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+
+                var result = await userManager.AddPasswordAsync(user, model.NewPassword);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+
+                await signInManager.RefreshSignInAsync(user);
+
+                return View("AddPasswordConfirmation");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
         public async Task<IActionResult> ChangePassword()
         {
+            var user = await userManager.GetUserAsync(User);
+
+            var userHasPassword = await userManager.HasPasswordAsync(user);
+
+            if (!userHasPassword)
+            {
+                return RedirectToAction("AddPassword");
+            }
+
             return View();
         }
 
